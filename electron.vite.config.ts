0cb71e49ff0d294ba6ae6856a11fcb9193979a7c
Deyweson <deywesonalm@gmail.com>
2024-08-09 10:10:19 -0300

    Feat Connected to Database :sparkles:

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,23 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [
+      externalizeDepsPlugin(),
+      commonjs({
+        // Ignorar requires dinâmicos (útil para módulos nativos)
+        ignoreDynamicRequires: true,
+
+        // Definir módulos específicos para serem tratados como requires dinâmicos
+        dynamicRequireTargets: [
+          // Inclua o caminho para o seu módulo nativo aqui
+          'C:/dev/Shiny-Count/build/node_sqlite3.node'
+        ]
+      })
+    ]
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
--- src/main/database.ts
+++ src/main/database.ts
@@ -1,4 +1,14 @@
 import { ipcMain } from 'electron'
+import sqlite from 'sqlite3'
+import path from 'node:path'
+
+const db = new sqlite.Database(path.join(__dirname, 'database.db'), (err) => {
+  if (err) {
+    return console.error(err.message)
+  }
+  console.log(path.join(__dirname, 'database.db'))
+  console.log('Connected to the SQlite database.')
+})
 
 export const setupDatabase = (): void => {
   ipcMain.on('teste', () => {
@@ -6,3 +16,10 @@ export const setupDatabase = (): void => {
     return
   })
 }
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message)
+  }
+  console.log('Close the database connection.')
+})
